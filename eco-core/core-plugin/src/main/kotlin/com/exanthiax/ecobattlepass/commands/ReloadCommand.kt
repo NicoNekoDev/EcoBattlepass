b26d7b327cecf324978407d4e5f734b0680b8f9f
NicoNeko <nicuch100@gmail.com>
2026-03-12 14:51:13 +0200

    Add initial folia support

--- eco-core/core-plugin/src/main/kotlin/com/exanthiax/ecobattlepass/commands/ReloadCommand.kt
+++ eco-core/core-plugin/src/main/kotlin/com/exanthiax/ecobattlepass/commands/ReloadCommand.kt
@@ -1,21 +1,29 @@
 package com.exanthiax.ecobattlepass.commands
 
 import com.exanthiax.ecobattlepass.plugin
+import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.PluginCommand
 import com.willfp.eco.util.NumberUtils
 import com.willfp.eco.util.StringUtils
 import org.bukkit.command.CommandSender
 
-object ReloadCommand: PluginCommand(
+object ReloadCommand : PluginCommand(
     plugin,
     "reload",
     "ecobattlepass.command.reload",
     false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        sender.sendMessage(
-            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                .replace("%time%", NumberUtils.format(plugin.reloadWithTime().toDouble()))
-        )
+        val runnable = Runnable {
+            sender.sendMessage(
+                plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                    .replace("%time%", NumberUtils.format(plugin.reloadWithTime().toDouble()))
+            )
+        }
+        if (Prerequisite.HAS_FOLIA.isMet) {
+            plugin.scheduler.runTask(runnable)
+        } else {
+            runnable.run()
+        }
     }
 }
