21af89c72426504a358570bd19b4e3027addc437
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-03-19 13:32:03 +0000

    Update reload command messages and improve reload time reporting

--- eco-core/core-plugin/src/main/kotlin/com/exanthiax/ecobattlepass/commands/ReloadCommand.kt
+++ eco-core/core-plugin/src/main/kotlin/com/exanthiax/ecobattlepass/commands/ReloadCommand.kt
@@ -1,9 +1,15 @@
 package com.exanthiax.ecobattlepass.commands
 
+import com.exanthiax.ecobattlepass.battlepass.BattlePasses
+import com.exanthiax.ecobattlepass.categories.Categories
 import com.exanthiax.ecobattlepass.plugin
+import com.exanthiax.ecobattlepass.quests.BattleQuests
+import com.exanthiax.ecobattlepass.rewards.Rewards
+import com.exanthiax.ecobattlepass.tasks.BattleTasks
 import com.willfp.eco.core.command.impl.PluginCommand
 import com.willfp.eco.util.NumberUtils
 import com.willfp.eco.util.StringUtils
+import com.willfp.eco.util.toNiceString
 import org.bukkit.command.CommandSender
 
 object ReloadCommand: PluginCommand(
@@ -15,7 +21,8 @@ object ReloadCommand: PluginCommand(
     override fun onExecute(sender: CommandSender, args: List<String>) {
         sender.sendMessage(
             plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                .replace("%time%", NumberUtils.format(plugin.reloadWithTime().toDouble()))
+                .replace("%time%", plugin.reloadWithTime().toNiceString())
+                .replace("%count%", BattlePasses.values().size.toString())
         )
     }
 }
